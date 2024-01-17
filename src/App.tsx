70ef3464bda866fbc14a55d3ef977405c0c4b3bd
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-01-17 14:45:11 +0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+export default function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
