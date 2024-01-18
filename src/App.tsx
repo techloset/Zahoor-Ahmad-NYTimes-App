0a76733561f26855c439d30b2cea3f98efb28306
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-01-18 09:27:47 +0500

    Master Branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-
 export default function App() {
   return (
     <div className="">
-      <Navbar />
-      <Footer />
+      <div>Hello World</div>
     </div>
   );
 }
