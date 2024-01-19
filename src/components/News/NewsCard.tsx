43a16e4a0be1c3923f098802d3bfc8e0db7cf430
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-01-19 13:15:43 +0500

    News in NewsSection fetched from api

--- src/components/News/NewsCard.tsx
+++ src/components/News/NewsCard.tsx
@@ -14,7 +14,7 @@ const NewsCard = ({
 }: NewsProps) => {
   return (
     // <div className="min-h-[500px] flex flex-col sm:gap-[76px]">
-    <div className="sm:h-520 sm:w-438 flex flex-col rounded-lg shadow-black/15 hover:shadow-black/20 shadow-md hover:shadow-lg border-black-2">
+    <div className="sm:h-[520px] sm:w-[438px] flex flex-col rounded-lg shadow-black/15 hover:shadow-black/20 shadow-md hover:shadow-lg border-black-2">
       <a href={url} target="_blank">
         <div className="sm:w-[438px] sm:h-[246px] h-[210px] overflow-hidden sm:mb-3.125rem rounded-t-lg">
           <img
@@ -33,17 +33,17 @@ const NewsCard = ({
             {description}
           </p>
         </div>
-        <div className="flex sm:gap-[9px] ml-[26px] mb-[10px] gap-1 ">
+        <div className="flex sm:gap-[9px] ml-[26px] mb-[5px] gap-1 ">
           <p className="sm:w-[94px] h-5 text-zinc-800 sm:text-[13px] font-normal font-Poppins leading-snu">
             {pubishedAt}
           </p>
           <p className="sm:h-5 opacity-70 text-zinc-800 sm:text-[13px] font-normal font-Poppins leading-snug">
-            {byLine} 
+            {byLine}
           </p>
         </div>
       </div>
       <div className="mx-auto w-full border-t-[1px] border-[#2a2a2a] border-opacity-[0.1] bg-transparent">
-        <div className="flex justify-center sm:gap-[50px] gap-[30px] mt-[10px] pb-[20px]">
+        <div className="flex justify-center sm:gap-[50px] gap-[30px] mt-[5px] pb-[20px]">
           <div className="flex items-center gap-[7px]">
             <IoHeartOutline />
             <span className="text-[10px]">29</span>
--- src/components/News/NewsSection.tsx
+++ src/components/News/NewsSection.tsx
@@ -1,138 +1,48 @@
+import { useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
 
+export interface TopNews {
+  _id: string;
+  title: string;
+  abstract: string;
+  url: string;
+  multimedia: { url: string; format: string }[];
+  published_date: string;
+  byline: string;
+}
+
 export default function NewsSection() {
+  const [articles, setArticles] = useState<TopNews[]>([]);
+  useEffect(() => {
+    const getNews = async () => {
+      const response = await fetch(
+        "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=g5w08PXd2Id8U1hqCGztsUCeqtqJzAKh"
+      );
+      const data = await response.json();
+      console.log(data.results);
+      setArticles(data.results);
+    };
+    getNews();
+  }, []);
+
   return (
-    <div className="sm:max-w-[1920px] sm:mx-0 mx-[16px] ">
+    <div className="sm:max-w-[1920px] sm:mx-0 mx-[16px] sm:mb-[50px]">
       <h3 className="font-bold text-3xl text-center">News Section</h3>
       <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-[26px] gap-[16px] ">
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-        <NewsCard
-          _id="1"
-          byLine="Zahoor Ahmad"
-          headline="John Lewis, civil rights giant, crosses infamous Selma bridge one final time"
-          description="Solemn crowds watch as Lewis, who died earlier this month at the age of 80, is borne by caisson over Edmund Pettus Bridge"
-          imageAlternative="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          imageSource="https://www.nytimes.com/images/2024/01/05/arts/05lythgoe-resigns/05lythgoe-resigns-articleLarge.jpg"
-          key={1}
-          pubishedAt="2024-01-05"
-          url="https://www.nytimes.com/2024/01/05/arts/television/nigel-lythgoe-sytycd-judge-sexual-assault.html"
-        />
-      </div>
-      {/* {articles.map((article) => {
-        const {
-          _id,
-          web_url,
-          headline: { main },
-          lead_paragraph,
-          byLine: { original },
-          multimedia: { url },
-          abstract,
-          pub_date,
-        } = article;
-        return (
+        {articles.map((article) => (
           <NewsCard
-            _id={_id}
-            key={_id}
-            byLine={original}
-            headline={main}
-            description={lead_paragraph}
-            url={web_url}
-            imageSource={`https://nytimes.com/${url}`}
-            imageAlternative={abstract}
-            pubishedAt={pub_date}
+            _id={article._id}
+            byLine={article.byline}
+            headline={article.title}
+            description={article.abstract}
+            imageSource={article.multimedia[0].url}
+            imageAlternative={article.multimedia[0].format}
+            key={article._id}
+            pubishedAt={article.published_date.split("T")[0]}
+            url={article.url}
           />
-        );
-      })} */}
-      {/* {articles.map((ar) => {
-        return <div>{ar.abstract}</div>;
-      })} */}
+        ))}
+      </div>
     </div>
   );
 }
