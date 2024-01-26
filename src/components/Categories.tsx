f6e7847003600f570c0d57a3470f3528ff7fd660
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-01-26 09:54:14 +0500

    deleted everything from styles

--- src/components/Categories.tsx
+++
@@ -1,7 +0,0 @@
-const Categories = () => {
-  return (
-    <div className="sm:w-[1368px] sm:h-[54px] bg-white bg-opacity-90 backdrop-blur-[28px]"></div>
-  );
-};
-
-export default Categories;
--- src/interfaces/TopNews.ts
+++
@@ -1,9 +0,0 @@
-export default interface TopNews {
-  _id: string;
-  title: string;
-  abstract: string;
-  url: string;
-  multimedia: { url: string; format: string }[];
-  published_date: string;
-  byline: string;
-}
