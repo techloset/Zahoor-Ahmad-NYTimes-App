9a30f7ee8140fcf95813262e5962956d2589d773
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-01-26 09:56:54 +0500

    deleted everything from master

--- src/interfaces/BreakingNewsType.ts
+++
@@ -1,9 +0,0 @@
-export default interface BreakingNewsType {
-  _id: string;
-  title: string;
-  abstract: string;
-  url: string;
-  multimedia: { url: string; format: string }[];
-  published_date: string;
-  byline: string;
-}
