b4641aa83ee80a4071e4ce3ebffaf9a8baa1d884
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-01-27 20:53:23 +0500

    news modal styling

--- src/components/news/NewsModal.tsx
+++ src/components/news/NewsModal.tsx
@@ -19,7 +19,6 @@ const NewsModal: React.FC<NewsModalProps> = ({
   description,
   imageSource,
   pubishedAt,
-  url,
   byLine,
   imageAlternative,
 }: NewsModalProps) => {
@@ -27,11 +26,11 @@ const NewsModal: React.FC<NewsModalProps> = ({
     return null;
   }
   return (
-    <div className="relative ">
-      <div className="p-[48px] min-h-[500px] flex flex-col sm:flex-row sm:gap-[76px] sm:mt-[100px] z-[200px] shadow-lg shadow-black bg-white sm:w-[1462px] fixed top-[10%] left-[10%] border rounded-[20px]">
+    <div className="h-[100vh] w-[100vw] flex justify-center items-center bg-black/40 fixed top-0 left-0">
+      <div className="p-[48px] rounded-[20px] min-h-[500px] flex flex-col sm:flex-row sm:gap-[76px] sm:mt-[100px] relative bg-white sm:w-[1462px] ">
         <div
           onClick={closeModal}
-          className="absolute top-4 right-4 hover:cursor-pointer"
+          className="hover:cursor-pointer absolute top-4 right-4"
         >
           <CrossIcon />
         </div>
@@ -50,11 +49,9 @@ const NewsModal: React.FC<NewsModalProps> = ({
             </div>
           </div>
           <div>
-            <a href={url} target="_blank">
-              <h3 className="sm:w-[540px] sm:text-zinc-800 text-white text-2xl w-[327px] sm:text-[32px] sm:mt-0 mx-[24px] sm:mx-0 sm:mb-2  font-semibold font-IBM z-50">
-                {headline}
-              </h3>
-            </a>
+            <h3 className="sm:w-[540px] sm:text-zinc-800 text-white text-2xl w-[327px] sm:text-[32px] sm:mt-0 mx-[24px] sm:mx-0 sm:mb-2  font-semibold font-IBM z-50">
+              {headline}
+            </h3>
             <p className="sm:w-[500px]  sm:block sm:h-[94px] text-zinc-800 sm:text-[15px] font-normal font-Poppins leading-normal">
               {description}
             </p>
